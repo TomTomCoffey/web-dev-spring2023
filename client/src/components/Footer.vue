0b2311b65b037906ce348f2fa4e6d8946358cfbf
TomTomCoffey <tomcoffey1017@yahoo.com>
2023-03-02 14:04:41 -0500

    Footer.vue

---
+++ client/src/components/Footer.vue
@@ -0,0 +1,14 @@
+<script setup lang= "ts">
+import { RouterLink, RouterView } from 'vue-router';
+
+const isMenuActive = false;
+
+</script>
+
+<template>
+
+
+
+</template>
+
+<style scoped></style>
