a8be78a75b0002b6c40af7b24e58e57741ea1b66
TomTomCoffey <tomcoffey1017@yahoo.com>
2023-03-21 14:06:27 -0400

    updated classwork

--- server/index.js
+++ server/index.js
@@ -1,17 +1,35 @@
-const http = require('http');  // Import the http module
+const express = require('express')
+const app = express() // Import the http module
 
 const hostname = '127.0.0.1';
 const port = process.env.PORT || 3000; // Port number 3000 is used for development
 
-const server = http.createServer((req, res) => { 
+app
+   .get('/', (req, res) => {
+  res.send('Hello World! From express!!')
+})
+  .get('/products', (req, res) => {
+    res.send({
+      items[
+        {
+          id: 1,
+          name: 'product 1'
+      }]
+    
+  })
 
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello New Paltz From NODEMON');
 
-});
 
-server.listen(port, () => 
-  console.log(`Server running at http://${hostname}:${port}/`)
-);
+
+
+
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
+
+
+
 
