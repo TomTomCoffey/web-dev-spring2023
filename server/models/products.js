b93ca1de3034b8ee6970ca148eb6901bce602640
TomTomCoffey <tomcoffey1017@yahoo.com>
2023-03-23 14:05:04 -0400

    catching up with class on server side

--- server/models/products.js
+++ server/models/products.js
@@ -1,45 +1,41 @@
 const data = require('../data/products.json');
 
 function getProducts() {
-
-    return data.products;
-    
+  return data.products;
 }
-function getProductbyId(id) {
-    product.id = data.products.length + 1;
-    return data.products.find(product => product.id == id);
-    
+
+function getProductById(id) {
+    return data.products.find(product => product.id === id);
 }
 
 function addProduct(product) {
+    product.id = data.products.length + 1;
     data.products.push(product);
 }
 
-function upDateProduct(id, product) {
-    const index = data.products.findIndex(product => product.id == id);
+function updateProduct(product) {
+    const index = data.products.findIndex(p => p.id === product.id);
     data.products[index] = product;
 }
+
 function deleteProduct(id) {
-    const index = data.products.findIndex(product => product.id == id);
+    const index = data.products.findIndex(p => p.id === id);
     data.products.splice(index, 1);
 }
 
 function searchProducts(searchTerm) {
-    return data.products.filter(product => product.name.toLowerCase().includes(searchTerm.toLowerCase()))||
-    product.description.toLowerCase().includes(searchTerm.toLowerCase()) ||
-    product.price.toLowerCase().includes(searchTerm.toLowerCase());
-
-
-
-
+    return data.products.filter(product => {
+        return  product.title.toLowerCase().includes(searchTerm.toLowerCase())  ||
+            product.description.toLowerCase().includes(searchTerm.toLowerCase())  ||
+            product.brand.toLowerCase().includes(searchTerm.toLowerCase());
+    });
 }
-
 
 module.exports = {
     getProducts,
-    getProductbyId,
+    getProductById,
     addProduct,
-    upDateProduct,
+    updateProduct,
     deleteProduct,
-
-}
+    searchProducts
+};
